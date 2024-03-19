b9c25bb7174aeffd34383b33e221de3cccad2ddb
pradeep.22320105@viit.ac.in <pradeep.22320105@viit.ac.in>
2024-03-19 14:26:25 +0000

    Created Form1.RowTemplate1; Edited settings and Form1

--- client_code/Form1/RowTemplate1/__init__.py
+++ client_code/Form1/RowTemplate1/__init__.py
@@ -1,9 +1,9 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import RowTemplate1Template
 from anvil import *
 
-class Form1(Form1Template):
+class RowTemplate1(RowTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-     
+
     # Any code you write here will run before the form opens.
