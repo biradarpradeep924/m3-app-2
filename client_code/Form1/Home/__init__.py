b8959adb24c194f783432290dd5c50d38b26593f
pradeep.22320105@viit.ac.in <pradeep.22320105@viit.ac.in>
2024-03-25 16:40:04 +0000

    Created Form1.Home; Edited settings and Form1; Deleted Form1.RowTemplate1

--- client_code/Form1/Home/__init__.py
+++ client_code/Form1/Home/__init__.py
@@ -1,26 +1,21 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import HomeTemplate
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Form1(Form1Template):
+class Home(HomeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-      
+
     # Any code you write here will run before the form opens.
-  
+
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    username = self.text_box_1.text
-    password = self.text_box_2.text
+    open_form("Form1") 
 
-    if username == "admin" and password == "admin123":
-              alert("successfully login") 
-              open_form("Form1.Home")
-    else:
-             alert("Invalid username or password. Please try again.")
-    
-
-     
+  def file_loader_1_change(self, file, **event_args):
+    """This method is called when a new file is loaded into this FileLoader"""
+    app_tables.document.add_row(media_file=media_file)
+  
